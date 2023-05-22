33c31ef2274b6b9484abb6249668f9e47dffd6eb
iness0 <iness0chan@gmail.com>
2023-05-22 17:15:42 +0300

    added a bit jquery

--- .flaskenv
+++ .flaskenv
@@ -5,7 +5,6 @@ MAIL_USE_TLS=1
 REDIS_URL=redis://redis:6379/0
 ELASTICSEARCH_LOGIN=elastic
 ELASTICSEARCH_URL=http://elasticsearch:9200
-DATABASE_URL=mysql+pymysql://muse_me:Mysql12404@mysql:3306/muse_me
 MYSQL_USER=muse_me
 MYSQL_DATABASE=muse_me
 MYSQL_RANDOM_ROOT_PASSWORD=yes
--- boot.sh
+++ boot.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 source venv/bin/activate
 while true; do
-    flask db migrate
+    flask db upgrade
     if [[ "$?" == "0" ]]; then
         break
     fi
-    echo Migration command failed, retrying in 5 secs...
+    echo Upgrade command failed, retrying in 5 secs...
     sleep 5
 done
-flask db upgrade
 flask translate compile
 exec gunicorn -b :5000 --access-logfile - --error-logfile - muse_me:app
